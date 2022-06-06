86e17cba2ff894c7e08accbc00abb96081d6e004
Gabby <gabriel_marquez@dlsu.edu.ph>
2022-06-06 16:38:28 +0800

    test

--- app.py
+++ app.py
@@ -2,10 +2,18 @@
 # heroku logs -a just-a-temp-flask --tail
 from flask import Flask, render_template
 import matplotlib
+import PIL
+from histogram_equalization import he
 
 app = Flask(__name__)
 
 @app.route("/", methods = ['POST', 'GET', 'PUT'])
 def index():
-    print(matplotlib.__name__)
     return render_template('root.html')
+
+@app.route("/he_test", methods = ['POST', 'GET', 'PUT'])
+def app_he():
+    print("Attempt to to he on " + "input.jpg")
+    he("input.jpg")
+    print("DONE!!!!")
+    return "Hello World!"
