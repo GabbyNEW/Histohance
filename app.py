01993892c5b2b4c8659928d0fcba8c6a56dbc12d
Gabby <gabriel_marquez@dlsu.edu.ph>
2022-06-06 10:38:47 +0800

    dude

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # heroku logs -a just-a-temp-flask --tail
 # heroku logs -a just-a-temp-flask --tail
 from flask import Flask, render_template
+import matplotlib
 
 app = Flask(__name__)
 
 @app.route("/", methods = ['POST', 'GET', 'PUT'])
 def index():
+    print(matplotlib.__name__)
     return render_template('root.html')
